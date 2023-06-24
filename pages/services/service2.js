02c9f2ab401b985e547acbd2d5d59a19d45cec4f
prajitlal4 <prajit.lal4@gmail.com>
2023-06-24 14:02:02 +0800

    changed padding top for mobile and computer

--- pages/services/service2.js
+++ pages/services/service2.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Service1() {
   return (
-    <div className="bg-white px-6 py-32 lg:px-8">
+    <div className="bg-white px-6 py-10 lg:px-8">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mine Site Auto Electrical Repairs for Heavy Machinery</h1>
         <p className="mt-6 text-xl leading-8">
