6b1e05c05e0014e4c4fec8a40c38748d9d8de984
prajitlal4 <prajit.lal4@gmail.com>
2023-07-30 10:32:29 +0800

    text white on hero button

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -24,7 +24,7 @@ export default function Hero() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href="#contact"
-                className="rounded-md bg-blue-400 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-lg hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                className="rounded-md bg-blue-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-lg hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Contact us now!
               </Link>
