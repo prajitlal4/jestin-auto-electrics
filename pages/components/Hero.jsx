777fa1e768d9cfefc0840c789fc160cc99979ac3
prajitlal4 <prajit.lal4@gmail.com>
2023-07-30 10:28:36 +0800

    adjustments to hero text to cater for new image

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -8,23 +8,23 @@ export default function Hero() {
         <Image
           src="/HeroImage.jpg"
           alt=""
-          className="absolute inset-0 -z-10 h-full w-full object-cover brightness-[.25]"
+          className="absolute inset-0 -z-10 h-full w-full object-cover brightness-[.75]"
           width={1920}
           height={1080}
           loading="lazy"
         />
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl pb-5">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl pb-5 drop-shadow-md">
               Perth&apos;s trusted Auto Electrical company
             </h1>
-            <p className="mt-6 text-lg leading-8 text-white font-medium">
+            <p className="mt-6 text-lg leading-8 text-white font-medium drop-shadow-md">
               Powering Perth&apos;s vehicles and heavy machinery with expert service and care.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href="#contact"
-                className="rounded-md bg-blue-400 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                className="rounded-md bg-blue-400 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-lg hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Contact us now!
               </Link>
