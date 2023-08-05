e7949299d754752893a2f37a5a0a035e1b3d5458
prajitlal4 <prajit.lal4@gmail.com>
2023-08-05 16:36:51 +0800

    added gallery section

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Feature from "./components/Feature"
 import Hero from "./components/Hero"
 import Review from "./components/Reviews"
 import Contact from "./components/Contact"
+import Gallery from "./components/Gallery"
 
 export default function PreRender() {
   return (
     <div>
       <Hero />
       <Feature />
+      <Gallery />
       <Review />
       <Contact />
     </div>
