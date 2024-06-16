9df193afcbf692237563cf00e8e722cda532f7ee
prajitlal4 <prajit.lal4@gmail.com>
2024-06-16 20:14:51 +0800

    fixed

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -64,7 +64,7 @@ export default function Contact() {
   };
 
   return (
-    <div id="contact" className="relative isolate bg-white -z-10">
+    <div id="contact" className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pb-10 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
